e5d9f8d1a865b45dd7e387c4d3f04113bb40e040
DontStopPrograming <01dyamond01@gmail.com>
2023-05-08 02:33:19 -0500

    span modified

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const ham = document.querySelector('.ham')
 const enlaces = document.querySelector('.nav-menu')
+const xIcon = document.querySelector('.x-icon')
 
 ham.addEventListener('click', () => {
     enlaces.classList.toggle('activado')
+
+    if(enlaces.classList.contains('activado')){
+        xIcon.textContent = 'X'
+    } else {
+        xIcon.textContent = ''
+    }
 })
