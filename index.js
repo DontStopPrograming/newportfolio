f3172f3d33af0316209516007c1bb3bba8559f5e
DontStopPrograming <01dyamond01@gmail.com>
2023-05-09 10:48:46 -0500

    nav modified

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
-const ham = document.querySelector('.ham')
-const enlaces = document.querySelector('.nav-menu')
-const barras = document.querySelector('.ham span')
+const ham = document.querySelector('.ham');
+const enlaces = document.querySelector('.nav-menu');
+const barras = document.querySelectorAll('.ham span');
 
- ham.addEventListener('click', () => {
-    enlaces.classList.toggle('activado')
-    barras.forEach(child => {child.classList.toggle('animado')})
-    ham.classList.toggle('girar')
- })
+ham.addEventListener('click', () => {
+    enlaces.classList.toggle('activado');
+    barras[0].classList.toggle('animado');
+    barras[1].classList.toggle('animado');
+    barras[2].classList.toggle('animado');
+    ham.classList.toggle('girar');
+});
 
