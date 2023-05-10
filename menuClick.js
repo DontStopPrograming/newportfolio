13e65f6b439b6086a987b03f144baf85316a1ec5
DontStopPrograming <01dyamond01@gmail.com>
2023-05-10 15:13:45 -0500

    without parallax html

---
+++ menuClick.js
@@ -0,0 +1,10 @@
+// const menuBtn = document.querySelector('.ham')
+// const navMenut = document.querySelector('.nav-menu')
+
+// menuBtn.addEventListener('click', () => {
+//     navMenut.classList.toggle('open')
+// })
+
+// navMenut.addEventListener('click', () =>  {
+//     navMenut.classList.remove('open')
+// })
