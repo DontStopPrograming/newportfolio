4b5cc0cb028056500d4fd1a8551d42da333ef174
DontStopPrograming <01dyamond01@gmail.com>
2023-05-16 00:41:18 -0500

    links nav

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const ham = document.querySelector('.ham');
 const enlaces = document.querySelector('.nav-menu');
 const barras = document.querySelectorAll('.ham span');
+const navLinks = document.querySelectorAll('.nav-menu li')
+const body = document.querySelector('body')
 
 ham.addEventListener('click', () => {
     enlaces.classList.toggle('activado');
@@ -8,5 +10,17 @@ ham.addEventListener('click', () => {
     barras[1].classList.toggle('animado');
     barras[2].classList.toggle('animado');
     ham.classList.toggle('girar');
+
 });
 
+navLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        enlaces.classList.remove('activado')
+        barras[0].classList.remove('animado')
+        barras[1].classList.remove('animado')
+        barras[2].classList.remove('animado')
+        ham.classList.remove('girar')
+
+        body.classList.toggle('menu-abierto')
+    })
+})
